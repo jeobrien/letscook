1545752380ada2e15c1ad2eccb14b796a9e5a5bf
Jessica O'Brien <j.e.obrien28@gmail.com>
2015-11-29 20:41:26 -0800

    Plans retrieving from db

--- client/app/mealPlan/planner.js
+++ client/app/mealPlan/planner.js
@@ -1,8 +1,9 @@
-angular.module('LetsCook.planner', [])
+angular.module('LetsCook.planner', ['ui.bootstrap'])
 
-.controller('PlannerController', function ($scope, Planner, Recipes) {
+.controller('PlannerController', function ($scope, Planner) {
   $scope.data = {};
-  $scope.plan = {
+  $scope.plans = [];
+  $scope.plans.plan = {
     meals: [],
     nutrients: {}
   };
@@ -12,16 +13,21 @@ angular.module('LetsCook.planner', [])
 
   $scope.getPlan = function () {
     Planner.getPlan($scope, $scope.data.calories).then(function (plan) {
-      $scope.plan.nutrients = plan.nutrients;
+      var meals = [];
       plan.meals.forEach(function (meal) {
         var url = (meal.image).slice(0, -4);
-        $scope.plan.meals.push({
+        meals.push({
           id: meal.id,
           title: meal.title,
           image: 'https://spoonacular.com/recipeImages/'+meal.image,
           url: 'https://spoonacular.com/recipes/'+url
         });
       });
+      var plan = {
+        nutrients: plan.nutrients,
+        meals: meals
+      };
+      $scope.plans.push(plan);
       $scope.loaded = true;
     })
     .catch(function (err) {
@@ -29,8 +35,7 @@ angular.module('LetsCook.planner', [])
     });
   };
   $scope.savePlanToDB = function () {
-    Planner.savePlanToDB($scope, $scope.plan).then(function (resp) {
-      $scope.DBResponse = resp;
+    Planner.savePlanToDB($scope, $scope.plans[0]).then(function (resp) {
       $scope.dbSaved = true;
     })
     .catch(function (err) {
@@ -39,11 +44,37 @@ angular.module('LetsCook.planner', [])
   };
   $scope.getPlansFromDB = function () {
     Planner.getPlansFromDB($scope).then(function (recipes) {
-      $scope.DBResult = recipes;
-      $scope.dbLoaded = true;
+      recipes.forEach(function (recipe) {
+        var temp = {
+          nutrients: recipe.plan.nutrients,
+          meals: recipe.plan.meals
+        };
+        $scope.plans.push(temp);
+      });
+      $scope.paginate();
+      $scope.loaded = true;
     })
     .catch(function (err) {
       console.error(err);
+    });
+  };
+  /// PAGINATION ==========================================
+  $scope.itemsPerPage = 5
+  $scope.currentPage = 1;
+
+  $scope.paginate = function () {
+    $scope.totalItems = $scope.plans.length;
+    $scope.$watch('currentPage + itemsPerPage', function() {
+      var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+      var end = begin + $scope.itemsPerPage;
+      $scope.filteredPlans = $scope.plans.slice(begin, end);
+    
+      var counter = $scope.plans.length / $scope.itemsPerPage;
+      if ($scope.currentPage <= counter) {
+        $scope.showing = $scope.itemsPerPage*$scope.currentPage;
+      } else {
+        $scope.showing = $scope.plans.length;
+      }
     });
   };
 });
